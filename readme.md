0b03a6ab74127f425c7d633ea9715bb40c9a5315
54696d21 <gabdaten@gmail.com>
2020-11-14 20:19:37 +0100

    initial commit

---
+++ readme.md
@@ -0,0 +1,21 @@
+# ESP8266 webhook example
+
+an easy way to trigger an event on a ESP8266 by making a http request to it (with the option of transmitting data to it).
+
+this code example implements 2 urls that can be called to trigger a function
+
+## Getting started
+
+1. git clone <this repo>
+
+2. fill in our wifi credentials 
+
+3. flash it on an ESP8266 either using the Arduino IDE (using the `WebhookExample.ino` file ) or PlatformIO (by opening this repo in PlatformIO)
+
+4. once the ESP8266 is connected to your local network, you can make a get request to  
+`<your ESP8266's IP>/webhook1` or `http://esp8266.local/webhook1`
+to execute a function
+
+and 
+
+`http://esp8266.local/webhook1?myArg1=HelloWorld` to execute the function and print out "HelloWorld" to the command line
---
+++ src/main.cpp
@@ -0,0 +1,75 @@
+#include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266mDNS.h>
+
+MDNSResponder mdns;
+
+const char* ssid = "*****************";
+const char* password = "*************";
+
+ESP8266WebServer server(80);
+
+void setup(void){
+
+  Serial.begin(115200);
+  WiFi.begin(ssid, password);
+  Serial.println("");
+
+  // Wait for connection
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  // serial output of connection details
+  Serial.println("");
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  if (mdns.begin("esp8266", WiFi.localIP())) {
+    Serial.println("MDNS responder started");
+  }
+
+  // this page is loaded when accessing the root of esp8266´s IP
+  server.on("/", [](){
+    server.send(200, "text/html", "ok");
+  });
+
+    server.on("/webhook1", []{ //lambda function syntax
+      server.send(200, "text/html", "ok");
+      Serial.println("webhook1 called");
+
+      if (server.hasArg("plain")== false){ //Check if body received
+            server.send(200, "text/plain", "Body not received");
+            Serial.println("no accepted http parameter found");
+            return;
+      }
+            String message = server.arg("myArg1");
+             Serial.println(message);
+    });
+
+    server.on("/webhook2", []{ //lambda function syntax
+      server.send(200, "text/html", "ok");
+      Serial.println("webhook2 called");
+
+      if (server.hasArg("plain")== false){ //Check if body received
+            server.send(200, "text/plain", "Body not received");
+            Serial.println("no accepted http parameter found");
+            return;
+      }
+            String message = server.arg("myArg2");
+            Serial.println(message);
+    });
+
+  server.begin();
+  Serial.println("HTTP server started");
+}
+
+void loop(void){
+  mdns.update();
+  server.handleClient();
+}
